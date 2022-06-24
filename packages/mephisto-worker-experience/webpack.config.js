5aabf00163714f02c4dc91c653527b603a8446e9
Etesam Ansari <etesam1@fb.com>
2022-06-24 10:40:25 -0400

    updated actions

--- packages/mephisto-worker-experience/webpack.config.js
+++ packages/mephisto-worker-experience/webpack.config.js
@@ -25,18 +25,6 @@ module.exports = {
     extensions: ["", ".js", ".jsx", ".css"],
   },
   externals: {
-    // Don't bundle react or react-dom
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React",
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM",
-    },
+    react: "react",
   },
 };
